cdec30b00566e5d5d8c0f9c219f5619460043167
atambekovna <atambekovna1411@gmail.com>
2022-07-05 18:54:22 +0600

    nav bar

--- src/App.js
+++ src/App.js
@@ -15,6 +15,14 @@ function App() {
           <input type='text' autoFocus/>
           <button>click</button>
         </form>
+        <nav>
+          <a href='#'>Home</a>
+          <a href='#'>About</a>
+          <p>alllldkemdkemdokddemdcnjnjcnjnjendjenndjendjcjd
+            kedkedkedkedekdji3jiekdmkedkndnjj4dn kedjkejdiejdie
+            kdmmmmmmmmmmemkdjfrni4jro4jdo4jfnnvj rfmrni4jor o4ko
+          </p>
+        </nav>
         <a
           className="App-link"
           href="https://reactjs.org"
