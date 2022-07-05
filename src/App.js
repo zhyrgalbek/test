cca011b4e369f5a6e48d7bb579d5f751b3c3d517
atambekovna <atambekovna1411@gmail.com>
2022-07-05 18:04:23 +0600

    aiperi end

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Aiperi</p>
+        <p>aktilek</p>
         <a
           className="App-link"
           href="https://reactjs.org"
